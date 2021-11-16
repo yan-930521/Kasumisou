33feab2d4c98723c9124aae03fa0fb5d1eb36671
彥成 <81685716+yan-930521@users.noreply.github.com>
2021-11-16 15:10:25 +0000

    delete test.json && sorting /tool

--- tool/set.js
+++ tool/set.js
@@ -2,6 +2,6 @@ const Database = require("@replit/database");
 const User = require('./User.js');
 const db = new Database();
 db.get("data").then((d)=>{
-  d["823885929830940682"] = new User();
+  //d["823885929830940682"] = new User();
   db.set("data", d);
 })
